66bc7105ea14d3fa60c0367912f9520fb98f4941
iosebi <Ioseb-1999>
2023-10-09 17:12:30 +0400

    New microservice app

---
+++ apps/orders/src/orders.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { CreateOrderRequest } from './dto/create-order.request';
+
+@Controller()
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Post()
+  async createOrder(@Body() request: CreateOrderRequest) {
+    return this.ordersService.createOrder(request);
+  }
+}
---
+++ apps/orders/src/orders.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { CreateOrderRequest } from './dto/create-order.request';
+import { OrdersRepository } from './orders.repository';
+
+@Injectable()
+export class OrdersService {
+
+  constructor (private readonly orderRepository: OrdersRepository){}
+
+  async createOrder(request: CreateOrderRequest){
+
+  }
+}
